d5cf6c7fed959d33d78fadcb488043f0f16e5a54
Vlad <vlxdtimchenko@gmail.com>
2022-09-06 14:11:55 +0300

    conflict

--- CalcProject/App/Calc.cs
+++ CalcProject/App/Calc.cs
@@ -11,7 +11,7 @@ namespace CalcProject.App
     {
         public void Run ()
         {
-            Console.WriteLine("Helo,, wordldokdw!!");
+            Console.WriteLine("conflict");
         }
     }
 }
